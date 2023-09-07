5fd1febdba479e751659476a281fbe1fbddaaaee
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-07 17:58:11 +0500

    C3

--- C3/build/script.js
+++ C3/build/script.js
@@ -1,13 +1,4 @@
 "use strict";
-// Assigning types of variables
-// let a: String = "25";
-// let nambo: Number = 3445;
-// let check: Boolean;
-// Another syntax for assigning variable types
-// let nambo = <String>"usama";
-// Assigning two types (Union Type)
-// let surname: String | Number;
-// surname = 245;
 const func = (n, m) => {
     console.log(n, m);
     return n * m;
--- C3/src/script.ts
+++ C3/src/script.ts
@@ -1,16 +1,3 @@
-// Assigning types of variables
-
-// let a: String = "25";
-// let nambo: Number = 3445;
-// let check: Boolean;
-
-// Another syntax for assigning variable types
-// let nambo = <String>"usama";
-
-// Assigning two types (Union Type)
-// let surname: String | Number;
-// surname = 245;
-
 const func = (n: number, m: number): number => {
   console.log(n, m);
   return n * m;
