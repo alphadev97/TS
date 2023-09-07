dbed7036e8264127b058e0a8e8c0d6a4b0895cf7
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-07 18:33:35 +0500

    C3

--- C3/src/script.ts
+++ C3/src/script.ts
@@ -1,10 +1,21 @@
 // Defining types from the beginning to avoid clutter (Called Type aliases)
 
-type userName = number | string;
+// type userName = number | string;
+
+// let a: userName;
+
+// const func = (n: number, m: number): number => {
+//   console.log(n, m);
+//   return n * m;
+// };
+
+// Function with type declaration
+
+type userName = (n: number, m: number) => number;
 
 let a: userName;
 
-const func = (n: number, m: number): number => {
+const func: userName = (n, m): number => {
   console.log(n, m);
   return n * m;
 };
