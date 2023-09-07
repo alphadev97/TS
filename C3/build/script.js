7556088b1ab22d356ab332d6704c124240b1fcfd
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-07 19:11:31 +0500

    C3

--- C3/build/script.js
+++ C3/build/script.js
@@ -1,7 +1,18 @@
 "use strict";
 // Defining types from the beginning to avoid clutter (Called Type aliases)
-let a;
-const func = (n, m) => {
-    console.log(n, m);
-    return n * m;
-};
+// type userName = number | string;
+// let a: userName;
+// const func = (n: number, m: number): number => {
+//   console.log(n, m);
+//   return n * m;
+// };
+// Function with type declaration
+// type userName = (n: number, m: number) => number;
+// let a: userName;
+// const func: userName = (n, m): number => {
+//   console.log(n, m);
+//   return n * m;
+// };
+// Array
+const arr = [12, 24, 65, 14];
+const arr2 = ["usama"];
--- C3/src/script.ts
+++ C3/src/script.ts
@@ -11,11 +11,16 @@
 
 // Function with type declaration
 
-type userName = (n: number, m: number) => number;
+// type userName = (n: number, m: number) => number;
 
-let a: userName;
+// let a: userName;
 
-const func: userName = (n, m): number => {
-  console.log(n, m);
-  return n * m;
-};
+// const func: userName = (n, m): number => {
+//   console.log(n, m);
+//   return n * m;
+// };
+
+// Array
+
+const arr: number[] = [12, 24, 65, 14];
+const arr2: string[] = ["usama"];
