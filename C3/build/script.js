588993002ec893857a3846f2560b871d5291b034
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-07 18:04:32 +0500

    C3

--- C3/build/script.js
+++ C3/build/script.js
@@ -1,4 +1,6 @@
 "use strict";
+// Defining types from the beginning to avoid clutter (Called Type aliases)
+let a;
 const func = (n, m) => {
     console.log(n, m);
     return n * m;
--- C3/src/script.ts
+++ C3/src/script.ts
@@ -1,3 +1,9 @@
+// Defining types from the beginning to avoid clutter (Called Type aliases)
+
+type userName = number | string;
+
+let a: userName;
+
 const func = (n: number, m: number): number => {
   console.log(n, m);
   return n * m;
