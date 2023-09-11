45bfedf66824fe89d44300945925e2c3ba4a3a05
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-11 20:00:14 +0500

    C7

---
+++ C7/src/script.ts
@@ -0,0 +1,122 @@
+// type FuncTyp = (n: number, m: number, l?: number) => number;
+
+// // Optional parameter
+
+// const func: FuncTyp = (n, m, l) => {
+//   if (typeof l === "undefined") return n * m;
+
+//   return n * m * l;
+// };
+
+// func(25, 23);
+
+// ------------------------------------------------
+
+// // Default parameter
+// type FuncTyp = (n: number, m: number, l?: number) => number;
+
+// const func: FuncTyp = (n, m, l = 20) => {
+//   return n * m * l;
+// };
+
+// func(25, 23);
+
+// ------------------------------------------------
+
+// // Rest Operator
+// type FuncTyp = (...m: number[]) => number[];
+
+// const func: FuncTyp = (...m) => {
+//   return m;
+// };
+
+// func(25, 23, 54, 41, 7);
+
+// ------------------------------------------------
+
+// function lol(n: number): number {
+//   return 45;
+// }
+
+// function with Object
+
+// interface Product {
+//   name: string;
+//   stock: number;
+//   price: number;
+//   photo: string;
+//   readonly id: string;
+// }
+
+// type getDataType = (product: Product) => void;
+
+// const getData: getDataType = (product): void => {
+//   console.log(product);
+// };
+
+// const productOne: Product = {
+//   name: "Macbook",
+//   stock: 46,
+//   price: 9999,
+//   photo: "samplephotoUrl",
+//   id: "gJHdGSdjkAGd",
+// };
+
+// getData(productOne);
+
+// Never Type
+
+// type themeMode = "light" | "dark";
+
+// const mode: themeMode = "light";
+
+// const errorHandler = (): never => {
+//   throw new Error();
+// };
+
+// -------------------------------------------
+
+// Classes
+
+class Player {
+  //   private height; // private property cannot access outside class
+  //   public weight;
+
+  public readonly id: string;
+
+  constructor(
+    private height: number,
+    public weight: number,
+    protected power: number
+  ) {
+    this.id = String(Math.random() * 100);
+  }
+
+  get getMyHeight(): number {
+    return this.height;
+  }
+
+  set changeHeight(val: number) {
+    this.height = val;
+  }
+}
+
+const abhi = new Player(100, 150, 23);
+console.log("Height", abhi.getMyHeight);
+abhi.changeHeight = 500;
+console.log("Height", abhi.getMyHeight);
+
+// class Player2 extends Player {
+//   special: boolean;
+//   constructor(height: number, weight: number, power: number, special: boolean) {
+//     super(height, weight, power);
+//     this.special = special;
+//   }
+//   getMyPower = () => this.power;
+// }
+
+// const abhi = new Player2(100, 150, 23, true);
+// console.log("Weight", abhi.weight);
+// console.log("Height", abhi.getMyHeight());
+// console.log("Power", abhi.getMyPower());
+// console.log("Id", abhi.id);
