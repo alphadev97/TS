13c79b8d828761299fbcdfbc84e9f22057eb50bf
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-13 01:47:40 +0500

    Update

--- Todo-ts/src/main.ts
+++ Todo-ts/src/main.ts
@@ -5,3 +5,60 @@ interface Todo {
   isCompleted: boolean;
   readonly id: string;
 }
+
+const todos: Todo[] = [];
+
+const todosContainer = document.querySelector(
+  ".todoContainer"
+) as HTMLDivElement;
+const todoInput = document.getElementsByName("title")[0] as HTMLInputElement;
+const myForm = document.getElementById("myForm") as HTMLFormElement;
+
+myForm.onsubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+
+  const todo: Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    id: String(Math.random() * 1000),
+  };
+
+  todos.push(todo);
+  todoInput.value = "";
+  renderTodo(todos);
+};
+
+const generateTodoItem = (title: string, isCompleted: boolean, id: string) => {
+  const todo: HTMLDivElement = document.createElement("div");
+  todo.className = "todo";
+
+  //    creating a checkbox
+  const checkBox: HTMLInputElement = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.className = "isCompleted";
+  checkBox.checked = isCompleted;
+  checkBox.onchange = () => {
+    paragraph.className = checkBox.checked ? "textCut" : "";
+  };
+
+  // creating P for title
+  const paragraph: HTMLParagraphElement = document.createElement("p");
+  paragraph.innerText = title;
+
+  // creating delete button
+  const btn: HTMLButtonElement = document.createElement("button");
+  btn.innerText = "X";
+  btn.className = "deleteBtn";
+
+  // Appending all to Todoitem
+  todo.append(checkBox, paragraph, btn);
+
+  todosContainer.append(todo);
+};
+
+const renderTodo = (todos: Todo[]) => {
+  todosContainer.innerText = "";
+  todos.forEach((item) => {
+    generateTodoItem(item.title, item.isCompleted, item.id);
+  });
+};
