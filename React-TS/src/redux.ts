883a45fcbbc73355dd2bee9a2f112703087e60d9
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-14 20:24:21 +0500

    ReduxTS

--- React-TS/src/redux.ts
+++ React-TS/src/redux.ts
@@ -1,3 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
-export const store = configureStore({ reducer: {} });
+// interface StateType {
+//   count: number;
+// }
+
+// const increment = createAction("increment");
+// const decrement = createAction("decrement");
+
+// const initialState: StateType = { count: 0 };
+
+// const rootReducer = createReducer(initialState, (builder) => {
+//   builder
+//     .addCase(increment, (state) => {
+//       state.count += 1;
+//     })
+//     .addCase(decrement, (state) => {
+//       state.count -= 1;
+//     });
+// });
+
+// export const store = configureStore({ reducer: rootReducer });
+
+// ----------------Create Slice --------------------
+
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+interface StateType {
+  count: number;
+}
+
+const initialState: StateType = { count: 0 };
+
+const rootSlice = createSlice({
+  name: "Counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const store = configureStore({ reducer: rootSlice.reducer });
