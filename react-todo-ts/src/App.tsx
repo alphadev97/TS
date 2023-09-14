9efb126a3575776e271e3f2262393c23411bf60a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-14 23:39:40 +0500

    Todo

--- react-todo-ts/src/App.tsx
+++ react-todo-ts/src/App.tsx
@@ -11,19 +11,25 @@ import TodoItem from "./components/TodoItem";
 import { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState<TodoItemType[]>([
-    {
-      title: "sfsdffsf",
-      isCompleted: false,
-      id: "sdfsdfsfg",
-    },
-  ]);
+  const [todos, setTodos] = useState<TodoItemType[]>([]);
+
+  const [title, setTitle] = useState<TodoItemType["title"]>("");
 
   const completeHandler = (id: TodoItemType["id"]): void => {
     alert(id);
   };
   const deleteHandler = (id: TodoItemType["id"]): void => {
     alert(id);
+  };
+
+  const submitHandler = (): void => {
+    const newTodo: TodoItemType = {
+      title,
+      isCompleted: false,
+      id: String(Math.random() * 1000),
+    };
+    setTodos((prev) => [...prev, newTodo]);
+    setTitle("");
   };
 
   return (
@@ -44,13 +50,19 @@ const App = () => {
           />
         ))}
       </Stack>
-      <TextField fullWidth label={"New Task"} />
+      <TextField
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        label={"New Task"}
+      />
       <Button
         sx={{
           margin: "1rem 0",
         }}
         fullWidth
         variant="contained"
+        onClick={submitHandler}
       >
         Add
       </Button>
