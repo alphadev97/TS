ed009acbcf1ab72f7a5bb22846e87203e8f6514b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-15 18:49:12 +0500

    Route

--- react-lang-ts/src/App.tsx
+++ react-lang-ts/src/App.tsx
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./components/Home"));
+const Learning = lazy(() => import("./components/Learning"));
+const Quiz = lazy(() => import("./components/Quiz"));
+const Result = lazy(() => import("./components/Result"));
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<Learning />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
   );
