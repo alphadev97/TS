f6d714a5a7f768b4c2e2f364e1866a115182478e
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-16 19:42:29 +0500

    Learning

--- react-lang-ts/src/components/Learning.tsx
+++ react-lang-ts/src/components/Learning.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const Learning = () => {
+  const [count, setCount] = useState<number>(0);
+  const params = useSearchParams()[0].get("language");
+
   return <div>Learning</div>;
 };
 
