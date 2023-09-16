9481d378a123130f4351382fec961ca4a5945a6d
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-16 19:29:21 +0500

    Home

--- react-lang-ts/src/components/Home.tsx
+++ react-lang-ts/src/components/Home.tsx
@@ -1,5 +1,36 @@
+import { Container, Typography } from "@mui/material";
+
+const laguages = [
+  {
+    name: "Japanese",
+    code: "js",
+  },
+  {
+    name: "Urdu",
+    code: "ur",
+  },
+  {
+    name: "Arabic",
+    code: "ar",
+  },
+  {
+    name: "Spanish",
+    code: "es",
+  },
+  {
+    name: "French",
+    code: "fr",
+  },
+];
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxWidth={"sm"}>
+      <Typography variant="h3" p={"2rem"} textAlign={"center"}>
+        Welcome, Begin journey of your learning!
+      </Typography>
+    </Container>
+  );
 };
 
 export default Home;
