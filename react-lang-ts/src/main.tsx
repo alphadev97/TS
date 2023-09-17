17c567850dba18ff52924f11e663fd8f938b1a8f
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-17 19:54:16 +0500

    Redux

--- react-lang-ts/src/main.tsx
+++ react-lang-ts/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline, createTheme } from "@mui/material";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 const theme = createTheme({
   palette: {
@@ -16,7 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
