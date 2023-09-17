c942a99e57e865dc0125ed4d530b96fc471be955
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-17 19:07:49 +0500

    Quiz

--- react-lang-ts/src/components/Quiz.tsx
+++ react-lang-ts/src/components/Quiz.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Quiz = () => {
   const [result, setResult] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+  const [ans, setAns] = useState<string>("");
+  const navigate = useNavigate();
 
   return <div>Quiz</div>;
 };
