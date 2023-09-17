49a5cc276e14ee415347dbd5b477dcd12f287d0b
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-17 21:07:13 +0500

    Learning

--- react-lang-ts/src/components/Learning.tsx
+++ react-lang-ts/src/components/Learning.tsx
@@ -3,18 +3,24 @@ import { Button, Container, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { translateWords } from "../utils/feature";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
+  clearState,
   getWordsFail,
   getWordsRequest,
   getWordsSuccess,
 } from "../redux/slices";
+import Loader from "./Loader";
 
 const Learning = () => {
   const [count, setCount] = useState<number>(0);
   const params = useSearchParams()[0].get("language") as LangType;
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { loading, error, words } = useSelector(
+    (state: { root: StateType }) => state.root
+  );
 
   const nextHandler = (): void => {
     setCount((prev) => prev + 1);
@@ -29,7 +35,14 @@ const Learning = () => {
       .catch((err) => {
         dispatch(getWordsFail(err));
       });
+
+    if (error) {
+      alert(error);
+      dispatch(clearState());
+    }
   }, []);
+
+  if (loading) return <Loader />;
 
   return (
     <Container
@@ -48,10 +61,10 @@ const Learning = () => {
       <Typography m={"2rem 0"}>Learning Made Easy</Typography>
       <Stack direction={"row"} spacing={"1rem"}>
         <Typography variant="h4">
-          {count + 1} - {"Sample"}
+          {count + 1} - {words[count]?.word}
         </Typography>
         <Typography color={"blue"} variant="h4">
-          : {"LOL"}
+          : {words[count]?.meaning}
         </Typography>
         <Button sx={{ borderRadius: "50%" }}>
           <VolumeUp />
@@ -63,9 +76,11 @@ const Learning = () => {
         }}
         variant="contained"
         fullWidth
-        onClick={count === 7 ? () => navigate("/quiz") : nextHandler}
+        onClick={
+          count === words.length - 1 ? () => navigate("/quiz") : nextHandler
+        }
       >
-        {count === 7 ? "Text" : "Next"}
+        {count === words.length - 1 ? "Text" : "Next"}
       </Button>
     </Container>
   );
