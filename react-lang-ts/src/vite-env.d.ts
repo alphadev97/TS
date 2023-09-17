f736eb6a75be73684603b5d651883d7733257785
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-17 20:02:22 +0500

    Types

--- react-lang-ts/src/vite-env.d.ts
+++ react-lang-ts/src/vite-env.d.ts
@@ -1,3 +1,16 @@
 /// <reference types="vite/client" />
 
 type LangType = "js" | "ur" | "ar" | "es" | "fr";
+
+type WordType = {
+  word: string;
+  meaning: string;
+  options: string[];
+};
+
+interface StateType {
+  loading: boolean;
+  result: string[];
+  words: WordType[];
+  error?: string;
+}
