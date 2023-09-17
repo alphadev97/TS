d14fa6154779f981cb9d28bbae9994ab54b74c42
Muhammad Usama <muhammadusama30289@gmail.com>
2023-09-17 19:50:08 +0500

    Quiz

--- react-lang-ts/src/components/Quiz.tsx
+++ react-lang-ts/src/components/Quiz.tsx
@@ -1,3 +1,13 @@
+import {
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -10,9 +20,51 @@ const Quiz = () => {
   const nextHandler = (): void => {
     setResult((prev) => [...prev, ans]);
     setCount((prev) => prev + 1);
+    setAns("");
   };
 
-  return <div>Quiz</div>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        padding: "1rem",
+      }}
+    >
+      <Typography m={"2rem 0"}>Quiz</Typography>
+      <Typography variant="h3">
+        {count + 1} - {"Randoms"}
+      </Typography>
+
+      <FormControl>
+        <FormLabel
+          sx={{
+            mt: "2rem",
+            mb: "1rem",
+          }}
+        >
+          Meaning
+        </FormLabel>
+        <RadioGroup value={ans} onChange={(e) => setAns(e.target.value)}>
+          <FormControlLabel
+            value={"LoL"}
+            control={<Radio />}
+            label={"Option 1"}
+          />
+        </RadioGroup>
+      </FormControl>
+      <Button
+        sx={{
+          margin: "3rem 0",
+        }}
+        variant="contained"
+        fullWidth
+        onClick={nextHandler}
+        disabled={ans === ""}
+      >
+        {count === 7 ? "Submit" : "Next"}
+      </Button>
+    </Container>
+  );
 };
 
 export default Quiz;
