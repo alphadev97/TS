799fa457dd0f41cbfebe2338662ad868be13fef0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-18 16:43:47 +0500

    Result

--- react-lang-ts/src/components/Result.tsx
+++ react-lang-ts/src/components/Result.tsx
@@ -1,4 +1,11 @@
-import { Container, List, ListItem, Stack, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  List,
+  ListItem,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 const result = ["lol", "Sample", "Ans"];
 
@@ -36,7 +43,31 @@ const Result = () => {
             ))}
           </List>
         </Stack>
+        <Stack>
+          <Typography m={"1rem 0"} variant="h5">
+            Correct Answer
+          </Typography>
+          <List>
+            {words?.map((i, idx) => (
+              <ListItem key={idx}>
+                {idx + 1} - {i.meaning}
+              </ListItem>
+            ))}
+          </List>
+        </Stack>
       </Stack>
+
+      <Typography
+        m={"1rem "}
+        variant="h5"
+        color={percentage > 50 ? "green" : "red"}
+      >
+        {percentage > 50 ? "Pass" : "Fail"}
+      </Typography>
+
+      <Button sx={{ margin: "1rem" }} variant="contained">
+        Reset
+      </Button>
     </Container>
   );
 };
