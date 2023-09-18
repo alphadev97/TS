10e9d3fd15580325f578e44ba73cc99b0283e3f8
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-09-18 20:50:34 +0500

    update

--- node-ts/dist/controllers/userController.js
+++
@@ -1,4 +0,0 @@
-export const getUser = (req, res, next) => {
-    const users = ["A", "B", "C", "D", "Lol"];
-    res.json({ users });
-};
--- node-ts/dist/routers/user.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { getUser } from "../controllers/userController.js";
-const router = express.Router();
-router.get("/all", getUser);
-export default router;
